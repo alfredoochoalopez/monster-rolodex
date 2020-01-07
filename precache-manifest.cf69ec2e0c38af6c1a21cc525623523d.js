2f86c8ddd7ffbce3ab685c72662e84ee90d55b74
Alfredo Ochoa <alfredo.ochoa@walmart.com>
2020-01-07 10:22:34 -0600

    Updates

---
+++ precache-manifest.cf69ec2e0c38af6c1a21cc525623523d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a9cbacaabf3acaf1cd57c279c7517a",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "21aedb3be060ba19d614",
+    "url": "/monster-rolodex/static/css/main.f48719ed.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21aedb3be060ba19d614",
+    "url": "/monster-rolodex/static/js/main.932ac6c1.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
